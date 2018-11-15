20e6ab5a28c2d69dad57809f1074fe903c649ac0
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-11-14 21:04:29 -0600

    test

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kudos_db', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/kudos_db', 
+{ useNewUrlParser: true });
 
 
 require('./routes/api-routes')(app);
