1e0b452590330fd85570e9be581e7020905ca4ae
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-11-15 09:48:13 -0600

    heroku

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const MONGODB_URI = require('./config/keys');
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kudos_db";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/kudos_db', 
-{ useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 require('./routes/api-routes')(app);
