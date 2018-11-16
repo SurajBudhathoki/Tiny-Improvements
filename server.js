1f183804e1b7c04fb7f5f7db0bc3833293e54953
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-11-15 18:11:49 -0600

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const MONGODB_URI =  "mongodb://localhost/kudos_db" || process.env.MONGODB_URI;
 
