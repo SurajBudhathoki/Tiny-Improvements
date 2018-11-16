618f3dc187e8d767873735f6475439fa9f56365b
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-11-15 18:03:37 -0600

    dbdbdbdb

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const MONGODB_URI =  "mongodb://localhost/kudos_db" || "mongodb://admin:pass123@ds263493.mlab.com:63493/kudos_db" ;
+const MONGODB_URI =  "mongodb://localhost/kudos_db" || process.env.MONGODB_URI;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
